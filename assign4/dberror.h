5e64d4553215100e642b1f5f8aee405c968af02b
Anusha Sonte Parameshwar <120292674+AnushaSP-iit@users.noreply.github.com>
2023-04-24 22:59:13 -0500

    added error codes

--- assign4/dberror.h
+++ assign4/dberror.h
@@ -14,6 +14,8 @@ typedef int RC;
 #define RC_FILE_HANDLE_NOT_INIT 2
 #define RC_WRITE_FAILED 3
 #define RC_READ_NON_EXISTING_PAGE 4
+#define RC_ERROR 400 // Added a new definiton for ERROR
+#define RC_PINNED_PAGES_IN_BUFFER 6 // Added a new definition for Buffer Manager
 
 #define RC_RM_COMPARE_VALUE_OF_DIFFERENT_DATATYPE 200
 #define RC_RM_EXPR_RESULT_IS_NOT_BOOLEAN 201
@@ -21,11 +23,23 @@ typedef int RC;
 #define RC_RM_NO_MORE_TUPLES 203
 #define RC_RM_NO_PRINT_FOR_DATATYPE 204
 #define RC_RM_UNKOWN_DATATYPE 205
+#define RC_SCHEMA_TOO_BIG 206
+#define RC_SCHEMA_TYPELENGTH_TOO_BIG 207
+#define RC_MEMORY_ALLOCATION_FAILED 208
+#define RC_NULL_ARGUMENT 209
 
 #define RC_IM_KEY_NOT_FOUND 300
 #define RC_IM_KEY_ALREADY_EXISTS 301
 #define RC_IM_N_TO_LAGE 302
 #define RC_IM_NO_MORE_ENTRIES 303
+
+// Added new definitions for Record Manager
+#define RC_RM_NO_TUPLE_WITH_GIVEN_RID 500
+#define RC_SCAN_CONDITION_NOT_FOUND 501
+
+// Added new definition for B-Tree
+#define RC_ORDER_TOO_HIGH_FOR_PAGE 601
+#define RC_NO_RECORDS_TO_SCAN 602
 
 /* holder for error messages */
 extern char *RC_message;
@@ -34,23 +48,24 @@ extern char *RC_message;
 extern void printError (RC error);
 extern char *errorMessage (RC error);
 
-#define THROW(_rc,_message)						\
-	do {										\
-		_RC_message=_message;					\
-		return _rc;								\
-	} while (0)									\
+#define THROW(rc,message) \
+  do {			  \
+    RC_message=message;	  \
+    return rc;		  \
+  } while (0)		  \
 
 // check the return code and exit if it is an error
-#define CHECK(_code)													\
-	do {																\
-		int _rc_internal = (_code);										\
-		if (_rc_internal != RC_OK)										\
-		{																\
-			char *_message = errorMessage(_rc_internal);				\
-			printf("[%s-L%i-%s] ERROR: Operation returned error: %s\n",__FILE__, __LINE__, __TIME__, _message); \
-			free(_message);												\
-			exit(1);													\
-		}																\
-	} while(0);
+#define CHECK(code)							\
+  do {									\
+    int rc_internal = (code);						\
+    if (rc_internal != RC_OK)						\
+      {									\
+	char *message = errorMessage(rc_internal);			\
+	printf("[%s-L%i-%s] ERROR: Operation returned error: %s\n",__FILE__, __LINE__, __TIME__, message); \
+	free(message);							\
+	exit(1);							\
+      }									\
+  } while(0);
+
 
 #endif
